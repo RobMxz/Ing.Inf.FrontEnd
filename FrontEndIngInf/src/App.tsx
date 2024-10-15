49d5ac16df897445c0b38803a5258158bdd7c34c
RobMxz <robmxz1234@gmail.com>
2024-10-15 14:04:42 -0500

    Router Done1

--- FrontEndIngInf/src/App.tsx
+++ FrontEndIngInf/src/App.tsx
@@ -1,13 +1,8 @@
-import { AgregarItemSede } from "./components/AuthForms/AgregarItemSede";
 import { LogIn } from "./components/AuthForms/LogIn";
-import { SignUp } from "./components/AuthForms/SignUp";
-import { VisualizarInventarioSede } from "./components/AuthForms/VisualizarInventarioSede";
 import Header from "./components/Header";
 import { Separator } from "@/components/ui/separator";
 
 import { ThemeProvider } from "@/components/theme-provider";
-import { EditarItem } from "./components/AuthForms/EditarItem";
-
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
